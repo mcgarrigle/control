102b37e77700c1d8ccfeed42836ba4611948ef1c
Peter McGarrigle <peter.mcgarrigle@bt.com>
2022-01-21 08:59:48 +0000

    added properties to profile::config_file

--- site/profile/manifests/config_file.pp
+++ site/profile/manifests/config_file.pp
@@ -1,38 +1,38 @@
 
-# Defines a config file transforming the supplied hash
+# Defines a config file transforming the supplied Hash
 # into a config file in either YAML or INI file
-# format
+# format.
 
 # @parameters:
 #
 # String $path: The name of the file to be created
-# String $format: Either yaml or inifile
+# String $format: Either 'yaml' or 'inifile'
 # String $owner: The owner of the file
-# String $group:
-# String $mode:
+# String $group: The group of the file
+# String $mode: The file permissions  
+# Hash   $content: A Hash contaiing the configuration
 
 # --------------------------------------------
 
 define profile::config_file(
-  String $path = $name,
+  String $path   = $name,
   String $format = 'yaml',
-  String $owner = 'nobody',
-  String $group = $owner,
-  String $mode  = '0644',
+  String $owner  = 'nobody',
+  String $group  = $owner,
+  String $mode   = '0644',
   Hash   $content,
 ){
-
-   if $format == 'yaml' {
-     file { $path:
-       content => to_yaml($content),
-       owner   => $owner,
-       group   => $group,
-       mode    => $mode,
-     }
+   case $format {
+     'yaml':    { $text = to_yaml($content) }
+     'inifile': { $text = hash2ini($content) }
+     'properties': { $text = hash2properties($content) }
+     default:   { fail("format ${format} not known") }
    }
 
-   if $format == 'inifile' {
-     $defaults = { path => $path }
-     inifile::create_ini_settings($content, $defaults)
+   file { $path:
+     content => $text,
+     owner   => $owner,
+     group   => $group,
+     mode    => $mode,
    }
 }
